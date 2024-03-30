bd65727b5846aa12d5b5fe57230029afe3d111a9
andiniswaa <145889023+andiniswaa@users.noreply.github.com>
2024-03-30 17:59:13 +0700

    Add files via upload

---
+++ Tugas-JS-PWEBPR-A-1077/scripts.js
@@ -0,0 +1,26 @@
+const content = document.getElementById("content")
+const title_content = document.getElementById("title-content");
+const desc_text = document.getElementById("desc-text");
+const box = document.getElementById("box")
+const desc_img = document.getElementById("desc-img");
+const button_group = document.getElementById("button-group");
+const button_true = document.getElementById("button-true");
+const button_false = document.getElementById("button-false");
+
+button_true.addEventListener('click', ()=> {
+  title_content.innerHTML = "Anda Berbohong!";
+  desc_text.innerHTML = "Anda adalah seorang Teknisi";
+  box.style.backgroundColor = "#E12E2E";
+  desc_img.src = "Album/Technician.svg";
+  button_group.style.display = 'none';
+  content.classList.add("animation-getar");
+});
+
+button_false.addEventListener('click', ()=> {
+  title_content.innerHTML = "Anda Benar!";
+  desc_text.innerHTML = "Anda adalah seorang Mahasiswa";
+  box.style.backgroundColor = "#6FD240"
+  desc_img.src = "Album/Colleger.svg";
+  button_group.style.display = 'none';
+  content.classList.add("animation-naikturun");
+});
